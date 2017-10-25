53c40fe2ef2b62a5a7caee8f1ddca7aeae754531
Ryan Lewis <ryandlewis.rl@gmail.com>
2017-10-25 14:03:26 -0500

    Finalized input validation

--- sieve.py
+++ sieve.py
@@ -1,8 +1,15 @@
 def inputFunction():
-    n = input("Please input a valid positive integer greater than 2: ")
-    if (not isinstance(n, int) or n < 2):
+    n = raw_input("Please input a valid positive integer greater than 2: ")
+    if (not check_value(n)):
         n = inputFunction()
     return n
+
+def check_value(some_value):
+    try:
+        y = int(some_value)
+    except ValueError:
+        return False
+    return y > 1 
 
 n = inputFunction()
 print(n)

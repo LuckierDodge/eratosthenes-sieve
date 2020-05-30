7422690e1e693b2ad9d2d88688990be715000ac5
Ryan Lewis <ryandlewis.rl@gmail.com>
2020-05-30 12:22:29 -0500

    Refactored for performance, readability, and autoformatting

--- sieve.py
+++ sieve.py
@@ -1,39 +1,41 @@
+#!/bin/python3
+
+### Main Wrapper
+def main():
+    print("\033c")
+    n = inputFunction()
+    print("Finding all prime numbers smaller than or equal to " + str(n) + "...")
+    final_list = runSieve(n)
+    print_counter = 0
+    [print(i) for i in final_list]
+    print("Number of primes between 2 and " + str(n) + ": " + str(len(final_list)))
+
+
 ### Get a positive integer greater than 2 from the user
 def inputFunction():
     n = input("Please input a valid positive integer greater than 2: ")
-    if (not checkValue(n)):
+    try:
+        y = int(n)
+    except ValueError:
+        y = -1
+    if not y > 2:
+        print("Looks like that's not a valid input, try again!")
         n = inputFunction()
     return int(n)
 
-### Validate input
-def checkValue(some_value):
-    try:
-        y = int(some_value)
-    except ValueError:
-        return False
-    return y > 2 
 
 ### Perform Sieve of Erasthosenes Algorithm
 def runSieve(n):
-    int_list = range(2,n+1)
-    mark_list = [False for i in range(2, n+1)]
-    prime_list = list(int_list)
-    for i in int_list:
-        if (mark_list[i-2] == False):
+    mark_list = [False for i in range(2, n + 1)]
+    for i in range(2, n + 1):
+        if not mark_list[i - 2]:
             j = 2
-            while i*j <= n:
-                if (mark_list[i*j-2] == False):
-                    mark_list[i*j-2] = True
-                    prime_list.remove(i*j)
-                j+=1
-    return prime_list
+            while i * j <= n:
+                if not mark_list[i * j - 2]:
+                    mark_list[i * j - 2] = True
+                j += 1
+    return [i for i in range(2, n + 1) if not mark_list[i - 2]]
 
-########### Main Wrapper ############
-print("\033c")
-n = inputFunction()
-print("Finding all prime numbers smaller than or equal to " + str(n) + "...")
-final_list = runSieve(n)
-print_counter = 0
-for i in final_list:
-    print(i)
-print("Number of primes between 2 and " + str(n) + ": " + str(len(final_list)))
+
+if __name__ == "__main__":
+    main()

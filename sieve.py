860c29ea94ad96f5153bd85af7fa8a8722ebf456
Ryan Lewis <ryandlewis.rl@gmail.com>
2020-08-26 14:52:41 -0500

    Add inline comments and code cleanup/refactoring

--- sieve.py
+++ sieve.py
@@ -2,11 +2,21 @@
 
 ### Main Wrapper
 def main():
+    # Clear the screen
     print("\033c")
+    # Get a number to sieve from the user
     n = inputFunction()
     print("Finding all prime numbers smaller than or equal to " + str(n) + "...")
-    final_list = runSieve(n)
-    print_counter = 0
+    # Perform sieve and store primes in final_list
+    mark_list = [False for i in range(2, n + 1)]
+    for i in range(2, n + 1):
+        if not mark_list[i - 2]:
+            j = 2
+            while i * j <= n:
+                if not mark_list[i * j - 2]:
+                    mark_list[i * j - 2] = True
+                j += 1
+    final_list = [i for i in range(2, n + 1) if not mark_list[i - 2]]
     [print(i) for i in final_list]
     print("Number of primes between 2 and " + str(n) + ": " + str(len(final_list)))
 
@@ -22,20 +32,6 @@ def inputFunction():
         print("Looks like that's not a valid input, try again!")
         n = inputFunction()
     return int(n)
-
-
-### Perform Sieve of Erasthosenes Algorithm
-def runSieve(n):
-    mark_list = [False for i in range(2, n + 1)]
-    for i in range(2, n + 1):
-        if not mark_list[i - 2]:
-            j = 2
-            while i * j <= n:
-                if not mark_list[i * j - 2]:
-                    mark_list[i * j - 2] = True
-                j += 1
-    return [i for i in range(2, n + 1) if not mark_list[i - 2]]
-
 
 if __name__ == "__main__":
     main()
